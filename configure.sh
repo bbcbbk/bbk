8136de2c6d1e53b254c5f319357ba9ad698f3156
bbcbbk <44605459+bbcbbk@users.noreply.github.com>
2021-03-24 16:58:22 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,6 +5,7 @@ mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
+xray -version
 
 # Remove temporary directory
 rm -rf /tmp/xray
@@ -32,7 +33,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws",
                 "allowInsecure": false,
                 "wsSettings": {
-                  "path": "/wo18cm"
+                  "path": "/wo18cm?ed=2048"
                 }
             }
         }
